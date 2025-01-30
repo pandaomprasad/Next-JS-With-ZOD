8533dc8610fcb89442fc0900e2c0401c1ecc933b
pandaomprasad <omprasadpanda1@gmail.com>
2025-01-30 21:36:57 +0530

    Schema Update

--- src/model/User.ts
+++ src/model/User.ts
@@ -15,6 +15,7 @@ export interface User extends Document {
   password: string;
   verifyCode: string;
   verifyCodeExpiry: Date;
+  isVerified:boolean;
   isAcceptingMessage: boolean;
   messages: Message[];
 }
@@ -29,5 +30,34 @@ const UserSchema: Schema<User> = new Schema({
     type: String,
     required: [true, "Email is Required"],
     unique: true,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/,
+      "Invalid Email",
+    ],
   },
+  password:{
+               type:String,
+               required:[true,"Password is Required"],
+  },
+  verifyCode: {
+               type: String,
+               required: [true, "Verify Code is Required"],
+  },
+  verifyCodeExpiry:{
+               type:Date,
+               required:[true,"Verify Code Expiry is Required"],
+  },
+  isVerified:{
+               type:Boolean,
+               default:false
+  },
+  isAcceptingMessage:{
+               type:Boolean,
+               default:true
+  },
+  messages:[MessageSchema]
 });
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserSchema)
+
+export default UserModel;
